4f48e51c98d376eb30d6bb07e8a54c4e2d9c0b24
Buranov Azamat <AABuranov@INT.HMCIS.RU>
2021-02-10 09:08:05 +0300

    Initial commit

---
+++ src/test/java/ru/natiel/xlsxeditor/XlsxeditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.natiel.xlsxeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsxeditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
